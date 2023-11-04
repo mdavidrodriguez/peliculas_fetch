8febec3089d0427097e56921fa2a2816404ef182
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-11-03 23:08:23 -0500

    api key peli

--- src/data/httpClient.jsx
+++ src/data/httpClient.jsx
@@ -2,7 +2,7 @@ const API = "https://api.themoviedb.org/3";
 export function get(path) {
   return fetch(API + path, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.API_KEY_URL}`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmE0MmRhYmI2ZTVmOTg2Mzk5MmExOGRhMzdlYWZhNCIsInN1YiI6IjY0MjljMzA0YjM5ZTM1MDA3N2NjNDkzNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.p_6n4Zjesznx7dC3Z-hD0NvQQ2Qhi9PUNIFkS5MLhuU`,
       "Content-Type": "application/json;charset=utf-8",
     },
   }).then((result) => result.json());
