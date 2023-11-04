ef7d95a663d2bd18f3417b9c2927748503613766
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-11-03 22:45:04 -0500

    config dotenv

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-
-dotenv.config(); // Carga las variables de entorno desde .env
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+})
